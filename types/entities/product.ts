2d1e42547980fc98a21d837be131d540d2d87cc1
sonnh <nguyenson20zzzz@gmail.com>
2025-12-16 16:01:56 +0700

    update entity

--- types/entities/product.ts
+++ types/entities/product.ts
@@ -1,61 +1,17 @@
 import { BaseEntity } from "./base/base";
-import { Category } from "./category";
-import { MediaBase } from "./media-base";
-import { ProductVariant } from "./product-variant";
-import { SubCategory } from "./subcategory";
-
-export interface ProductRepresentativeByCategory {
-  category?: Category | null;
-  product?: ProductRepresentative | null;
-}
-
-export interface ProductRepresentative {
-  sku?: string | null;
-  slug?: string | null;
-  src?: string | null;
-}
+import { OrderItem } from "./order-item";
 
 export enum ProductStatus {
-  Active,
-  Draft,
-  Archived,
+  Available,
+  OutOfStock,
+  Discontinued,
+  ComingSoon,
 }
 
 export interface Product extends BaseEntity {
-  sku?: string;
   name: string;
-  slug?: string;
-  categoryId?: string;
-  subCategoryId?: string;
-  thumbnailId?: string;
-  description?: string;
-  material?: string;
+  price: number;
   status: ProductStatus;
 
-  category?: Category;
-  subCategory?: SubCategory;
-  thumbnail?: MediaBase;
-  variants: ProductVariant[];
-}
-
-export interface ProductPreview extends BaseEntity {
-  sku?: string;
-  name: string;
-  slug?: string;
-  categoryId?: string;
-  subCategoryId?: string;
-  thumbnailId?: string;
-  description?: string;
-  material?: string;
-  status: ProductStatus;
-
-  minPrice: number;
-  maxPrice: number;
-  totalStockDefaultQuantity: number;
-  totalStockQuantity: number;
-
-  category?: Category;
-  subCategory?: SubCategory;
-  thumbnail?: MediaBase;
-  variants: ProductVariant[];
+  orderItems: OrderItem[];
 }
